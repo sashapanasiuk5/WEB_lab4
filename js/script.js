c106d0400a2efa83e202fd1da4becc9e43503b27
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2023-11-02 18:05:37 +0200

    Add animations

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+let hamburgerButton = document.querySelector(".hamburger-button");
+let menu = document.querySelector(".menu-wrapper");
+
+
+
+hamburgerButton.addEventListener("click", function (e) {
+
+	let buttonClasses = hamburgerButton.classList;
+	let menuClasses = menu.classList;
+	if(buttonClasses.contains('pressed')){
+		buttonClasses.remove('pressed');
+		buttonClasses.add('unpressed');
+
+		menuClasses.add('closed');
+		menuClasses.remove('opened');
+	}else{
+		buttonClasses.add('pressed');
+		menuClasses.add('opened');
+
+		buttonClasses.remove('unpressed');
+		menuClasses.remove('closed');
+	}
+	
+})
